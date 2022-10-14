75b98bc6bd52bfd86f366cb7133769cf32e2f302
5-16 <5-16@DESKTOP-1L7OMI7>
2022-10-14 21:09:18 +0500

    Добавьте файлы проекта.

---
+++ EmployerTime.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+
+namespace Indexers_lab
+{
+    enum DayOfWeek
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    }
+    class EmployerTime : ICounter, IWhenCreated//, IWriteToDisk // Sdelay sokhraneniye v fayl
+    {
+        private int[] WorkTime;
+        private int summOfHours;
+        private DateTime CreatedTime;
+        private string name;
+        public string Name { get; }
+        public DateTime Created
+        {
+            get
+            {
+                Console.WriteLine($"Объект {this.ToString()} был создан {CreatedTime.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+                return CreatedTime;
+            }
+        }
+        public int count
+        {
+            get
+            {
+                foreach (var item in WorkTime)
+                {
+                    summOfHours += item;
+                }
+                return summOfHours;
+            }
+        }
+        public void BriefPrint()
+        {
+            foreach (var item in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Console.WriteLine($"{item} - {WorkTime[(int)item]}");
+            }
+        }
+        public EmployerTime(string _name)
+        {
+            WorkTime = new int[7];
+            summOfHours = 0;
+            CreatedTime = DateTime.Now;
+            this.name = _name;
+        }
+        public EmployerTime()
+        {
+            WorkTime = new int[7];
+            summOfHours = 0;
+            CreatedTime = DateTime.Now;
+            this.name = "John Dow #" + CreatedTime.ToString();
+        }
+        public int this[int index]
+        {
+            set { WorkTime[index] = value; }
+            get { return WorkTime[index]; }
+        }
+        public void Print()
+        {
+            foreach (var item in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Console.WriteLine($"В день {item} отработано {WorkTime[(int)item]}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Indexers_lab
+{
+    interface ICounter
+    {
+        int count { get; }
+        void BriefPrint();
+    }
+    interface IWhenCreated
+    {
+        DateTime Created { get; }
+    }
+    interface IWriteToDisk
+    {
+        string filename { get; }
+        bool SaveToDisk();
+        bool ReadFromDisk();
+    }
+    class Program
+    {
+        public static string[] _weekDays = new string[] { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+        public static void MyInput(EmployerTime EmpTime)
+        {
+            bool TF = true;
+            do
+            {
+                try
+                {
+                    for (int i = 0; i < 7; i++)
+                    {
+                        Console.WriteLine($"Введите сколько часов проработал в {_weekDays[i]}");
+                        EmpTime[i] = int.Parse(Console.ReadLine());
+                        if (EmpTime[i] > 8)
+                        {
+                            EmpTime[i] = 8;
+                        }
+                        else
+                        {
+                            if (EmpTime[i] < 0)
+                            {
+                                EmpTime[i] = 0;
+                            }
+                        }
+                    }
+                    TF = false;
+                }
+                catch
+                {
+                    Console.WriteLine("ОШИБКА!ПОПРОБУЙТЕ ЕЩЁ РАЗ!");
+                }
+            } while (TF);
+
+            EmpTime.Print();
+            Console.WriteLine($"\nОтработано {EmpTime.count} часов");
+            Console.WriteLine(EmpTime.Created);
+        }
+        static void Main(string[] args)
+        {
+            var BobTime = new EmployerTime();
+            MyInput(BobTime);
+            var JohnTime = new EmployerTime();
+            MyInput(JohnTime);
+            Console.ReadKey();
+        }
+    }
+}
